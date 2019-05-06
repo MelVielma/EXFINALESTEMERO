6691906cdfff8d0421d9b8e4f98bd58d6ce02612
Melanie Vielma Saldaña <melanievielma@hotmail.com>
2019-05-06 18:24:23 -0500

    Intento de backend

---
+++ src/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV === 'production'){
+	var connectionURL = process.env.connectionURL
+}
+else{
+	const supersecreto = require('../config.js')
+	var connectionURL = supersecreto.connectionURL
+}
+
+mongoose.connect( connectionURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
