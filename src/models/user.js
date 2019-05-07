f483eae031eb2b6279ec5a0385cac49ec3fef7e6
Melanie Vielma Saldaña <melanievielma@hotmail.com>
2019-05-06 19:50:14 -0500

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,13 @@ const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+if(process.env.NODE_ENV === 'production'){
+  var secreto = process.env.secret
+}
+else{
+  const config = require('../config.js')
+  var secreto = config.secret
+}
 
 const userSchema = new mongoose.Schema({
   name: {
